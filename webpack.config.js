889cbe9f516fdedf2d942b88e0b1dcd05df374d8
Bart Slot <bartslot@gmail.com>
2019-12-17 13:47:42 +0100

    v0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,10 @@
-const {DllReferencePlugin} = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
 module.exports = [{
-  mode: 'development',
-  entry: ['./app.scss', './app.js'],
+  entry: './app.scss',
   output: {
-    filename: 'bundle.js'
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
   },
-  devServer: {
-    // Hidden on glitch.com.
-    contentBase: 'target'
-  },
-  plugins: [
-    // Run `npm run dll` to rebuild the components static file.
-    new DllReferencePlugin({
-      context: __dirname,
-      manifest: require('./target/components.dll.manifest.json'),
-      name: 'components'
-    }),
-
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      domain: process.env.PROJECT_DOMAIN,
-      inject: false
-    })
-  ],
   module: {
     rules: [
       {
@@ -39,22 +18,9 @@ module.exports = [{
           },
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
-          {
-            loader: path.resolve('fast-sass-loader.js'),
-            options: {
-              includePaths: ['./node_modules']
-            }
-          }
+          { loader: 'sass-loader' },
         ]
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-object-assign']
-        },
       }
     ]
-  }
+  },
 }];
